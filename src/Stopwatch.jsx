5f1120b3ff75dd43690855aeb316c2226b643c3c
durgeshpoakle7 <durgeshpokale7@gmail.com>
2025-06-17 21:53:50 +0530

    stopwatch

---
+++ src/Stopwatch.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useRef, useState } from "react"
+
+export default function Stopwatch(){
+    const[sec,setSec]=useState(65);
+
+    const Id=useRef(null);
+
+    
+  
+
+    const[on,setOn]=useState(false);
+    const Stopwatch=(e)=>{
+        e.preventDefault();
+        setOn((prev)=>!prev);
+    }
+
+    useEffect(()=>{
+        if(on){
+            Id.current=setInterval(() => {
+            setSec((prevSec)=>prevSec+1)
+        }, 1000);
+        }else{
+            clearInterval(Id.current);
+        }
+
+    
+        return ()=>clearInterval(Id.current);
+
+    },[on]);
+
+    const getTime=()=>{
+        const min=Math.floor(sec/60);
+        const seconds=sec%60;
+            return (`Time: ${min}:${seconds<10?"0":""}${seconds}`)
+    }
+        
+    
+
+
+    const reset=()=>{
+        clearInterval(Id.current);
+        setOn(false);
+        setSec(0);
+    }
+    return(
+        <div>
+            <h1>Stopwatch</h1>
+            {getTime()} 
+            <div>
+                <button onClick={Stopwatch}>{!on?("Start"):("Stop")}</button>
+                <button onClick={reset}>Reset</button>
+            </div>
+        </div>
+    )
+}
